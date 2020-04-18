9b9920333815f0a42bfcb21259255794b1a6382b
Paul Borm <mail@paulborm.de>
2020-04-18 12:36:27 +0200

    Add new sound 'Du Birne'

--- api/sounds.js
+++ api/sounds.js
@@ -6,200 +6,211 @@ const data = [
     id: "Rb3vNLwuv6410bled1n9P0qm",
     name: "Fuck",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/fuck.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/fuck.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/puppy.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/puppy.jpg`,
+    },
   },
   {
     id: "61w3P9vmsdiBgwd7865dqEoP",
     name: "Ha Ha",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/ostrich-laugh.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/ostrich-laugh.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/ostrich.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/ostrich.jpg`,
+    },
   },
   {
     id: "0e2q7Ou27bPkti6kMp5l2Ird",
     name: "Alles bleibt wie es ist",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/alles-bleibt-wie-es-ist.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/alles-bleibt-wie-es-ist.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/andreas.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/andreas.jpg`,
+    },
   },
   {
     id: "bJ8pBvdC2oxz028t1ua5mw0W",
     name: "NEIN NEIN NEIN",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/nein-nein-nein.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/nein-nein-nein.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/hitler.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/hitler.jpg`,
+    },
   },
   {
     id: "j5rycgGx7j67Z64xykh2w6JT",
     name: "Kannst kotzen gehen",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/kannst-kotzen-gehen.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/kannst-kotzen-gehen.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/sasha.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/sasha.jpg`,
+    },
   },
   {
     id: "Xas5LXkwR76n10c5ti29no",
     name: "Alle",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/alle.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/alle.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/alle.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/alle.jpg`,
+    },
   },
   {
     id: "bq9l6f0s23BhPaigv4Czq73H",
     name: "Bullshit",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/bullshit.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/bullshit.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/bullshit.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/bullshit.jpg`,
+    },
   },
   {
     id: "Nokm40k19fjM95MEd9an8afn",
     name: "Flex",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/flex.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/flex.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/flex.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/flex.jpg`,
+    },
   },
   {
     id: "Hfzc1FW3ba3nzhp6sY9b536d",
     name: "Mhmm",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/mhmm.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/mhmm.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/mhmm.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/mhmm.jpg`,
+    },
   },
   {
     id: "364kPi89uqmPq40Va5xdfaEw",
     name: "Mhmm 2",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/mhmm-2.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/mhmm-2.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/mhmm-2.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/mhmm-2.jpg`,
+    },
   },
   {
     id: "8778tTvpZnb7yqZg7ws3W5al",
     name: "Windows Error",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/windows-xp-error.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/windows-xp-error.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/windows.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/windows.jpg`,
+    },
   },
   {
     id: "RRF8ysfsNRQPGpequtwq7RBm",
     name: "ba dum tss",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/ba-dum-tss.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/ba-dum-tss.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/ba-dum-tss.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/ba-dum-tss.jpg`,
+    },
   },
   {
     id: "2n3bZaZNNJDZXpbLEtwBapx8",
     name: "DAMN",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/damn.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/damn.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/damn.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/damn.jpg`,
+    },
   },
   {
     id: "E34P6HaM2zeKZDBynCQbK2Zd",
     name: "ohhhhhh",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/ohhhhhh.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/ohhhhhh.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/ohhhhhh.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/ohhhhhh.jpg`,
+    },
   },
   {
     id: "BAa7BTpwBqmTPzUqKwaZywnP",
     name: "WOW",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/wow.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/wow.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/wow.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/wow.jpg`,
+    },
   },
   {
     id: "GfPVegp3FdaeXrmjfzu87HNs",
     name: "Fake news",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/you-are-fake-news.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/you-are-fake-news.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/you-are-fake-news.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/you-are-fake-news.jpg`,
+    },
   },
   {
     id: "xaXanTX2f5En8QQ8HUVp2UmG",
     name: "Noice",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/noice.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/noice.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/noice.jpg`
-    }
+      src: `${process.env.BASE_URL}/static/images/noice.jpg`,
+    },
   },
   {
     id: "ErhdWH8gkGRscgs9Lz3MU9qN",
     name: "doot",
     audio: {
-      src: `${process.env.BASE_URL}/static/sounds/doot.mp3`
+      src: `${process.env.BASE_URL}/static/sounds/doot.mp3`,
     },
     image: {
       alt: "",
-      src: `${process.env.BASE_URL}/static/images/doot.jpg`
-    }
-  }
+      src: `${process.env.BASE_URL}/static/images/doot.jpg`,
+    },
+  },
+  {
+    id: "pbldfymeymuFhnbbssiacavY",
+    name: "Du Birne",
+    audio: {
+      src: `${process.env.BASE_URL}/static/sounds/du-birne.mp3`,
+    },
+    image: {
+      alt: "",
+      src: `${process.env.BASE_URL}/static/images/du-birne.jpg`,
+    },
+  },
 ];
 
 router.get("/", (_req, res) => {
@@ -207,7 +218,7 @@ router.get("/", (_req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  const found = data.find(item => item.id === req.params.id);
+  const found = data.find((item) => item.id === req.params.id);
 
   if (found) {
     res.status(200).json(found);

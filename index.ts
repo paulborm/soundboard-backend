6864f8a5b2b6328a810297ef35318fa97c30ce22
Paul Borm <mail@paulborm.de>
2022-09-16 16:24:57 +0200

    do it!

--- index.ts
+++ index.ts
@@ -85,126 +85,68 @@ console.log(`Server listening on port ${port}`);
 
 await server.serve(Deno.listen({ port }));
 
-function channelHandler(event: any, socket?: WebSocket) {
-  if (event.target !== channel && event.type === "message") {
+function channelHandler(event: any) {
+  console.log("channel:message", event);
+
+  if (event.target !== channel) {
+    console.log("channel:message:postmessage", event);
     channel.postMessage(
       JSON.stringify(event.data),
     );
   }
 
-  if (!socket) return;
+  console.log("channel:message:further", event);
 
   if (event.type === "close") {
-    const user = state.getUser(socket);
+    const data = JSON.parse(event.detail);
 
-    if (user) {
-      state.deleteUser(user);
-
-      state.users.forEach((_, ws) => {
-        if (ws !== socket) {
-          ws.send(
-            JSON.stringify({
-              type: "userleft",
-              user,
-            }),
-          );
-        }
-      });
-    }
+    state.users.forEach((_, ws) => {
+      ws.send(
+        JSON.stringify({
+          type: "userleft",
+          user: data.user,
+        }),
+      );
+    });
 
     return;
   }
 
-  const data = JSON.parse(event.data);
-
   if (event.type === "message") {
+    const data = JSON.parse(event.data);
+
     if (data.type === "adduser") {
-      const user = state.addUser(
-        socket,
-        new User(crypto.randomUUID(), data.name),
-      );
-
-      socket.send(
-        JSON.stringify({
-          type: "userlogin",
-          user,
-          users: Array.from(state.users.values()),
-        }),
-      );
-
       state.users.forEach((_, ws) => {
-        if (ws !== socket) {
-          ws.send(
-            JSON.stringify({
-              type: "userjoined",
-              user,
-            }),
-          );
-        }
+        ws.send(
+          JSON.stringify({
+            type: "userjoined",
+            user: data.user,
+          }),
+        );
       });
     }
 
     if (data.type === "sound") {
       state.users.forEach((_, ws) => {
-        if (ws !== socket) {
-          ws.send(
-            JSON.stringify({
-              type: "sound",
-              sound: data.sound,
-              user: data.user,
-            }),
-          );
-        }
+        ws.send(
+          JSON.stringify({
+            type: "sound",
+            sound: data.sound,
+            user: data.user,
+          }),
+        );
       });
     }
 
     if (data.type === "updateuser") {
-      const { username } = data;
-
-      if (
-        !username ||
-        username.length <= 0 ||
-        username.length > 46 ||
-        typeof username !== "string"
-      ) {
-        return;
-      }
-
-      const user = state.getUser(socket);
-
-      if (user) {
-        user.name = username;
-
-        state.users.forEach((_, ws) => {
-          if (ws !== socket) {
-            ws.send(
-              JSON.stringify({
-                type: "userupdated",
-                user,
-              }),
-            );
-          }
-        });
-      }
-    }
-
-    if (event.type === "close") {
-      const user = state.getUser(socket);
-
-      if (user) {
-        state.deleteUser(user);
-
-        state.users.forEach((_, ws) => {
-          if (ws !== socket) {
-            ws.send(
-              JSON.stringify({
-                type: "userleft",
-                user,
-              }),
-            );
-          }
-        });
-      }
+      state.users.forEach((_, ws) => {
+        ws.send(
+          JSON.stringify({
+            type: "userupdated",
+            user: data.user,
+          }),
+        );
+      });
     }
   }
 }
@@ -273,12 +215,73 @@ async function handler(request: Request) {
     });
 
     socket.addEventListener("message", (event) => {
-      channelHandler(event, socket);
+      const data = JSON.parse(event.data);
+
+      if (data.type === "adduser") {
+        const user = state.addUser(
+          socket,
+          new User(crypto.randomUUID(), data.name),
+        );
+
+        socket.send(
+          JSON.stringify({
+            type: "userlogin",
+            user,
+            users: Array.from(state.users.values()),
+          }),
+        );
+
+        data.user = user;
+      }
+
+      if (data.type === "updateuser") {
+        const { username } = data;
+
+        if (
+          !username ||
+          username.length <= 0 ||
+          username.length > 46 ||
+          typeof username !== "string"
+        ) {
+          return;
+        }
+
+        const user = state.getUser(socket);
+
+        if (user) {
+          user.name = username;
+          data.user = user;
+        }
+      }
+
+      channelHandler(
+        new MessageEvent("message", { data: JSON.stringify(data) }),
+      );
     });
 
     socket.addEventListener("close", (event) => {
       console.log("[EVENT: disconnect]", socket);
-      channelHandler(event, socket);
+
+      const user = state.getUser(socket);
+
+      if (user) {
+        state.deleteUser(user);
+
+        state.users.forEach((_, ws) => {
+          if (ws !== socket) {
+            ws.send(
+              JSON.stringify({
+                type: "userleft",
+                user,
+              }),
+            );
+          }
+        });
+      }
+
+      channelHandler(
+        new MessageEvent("close", { data: JSON.stringify({ user }) }),
+      );
     });
 
     socket.addEventListener("error", () => {});
